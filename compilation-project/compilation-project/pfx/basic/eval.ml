5c72d143bc8d15e7e096aa724a75a74ce85004b2
paula.alvarado__ <paulam.alvarador@gmail.com>
2026-03-11 11:51:18 +0100

    function step in eval.ml

--- compilation-project/compilation-project/pfx/basic/eval.ml
+++ compilation-project/compilation-project/pfx/basic/eval.ml
@@ -14,7 +14,28 @@ let step state =
   match state with
   | [], _ -> Error("Nothing to step",state)
   (* Valid configurations *)
-  | DefineMe :: q , stack          -> Ok (q, stack)
+  | Push n :: q , stack          -> Ok (q, stack)
+  | Pop :: q, _ :: stack -> Ok (q, stack)
+  | Swap :: q, x :: y :: stack -> Ok (q, y :: x :: stack)
+  | Add :: q, x :: y :: stack -> Ok (q, (x + y):: stack)
+  | Sub :: q, x :: y :: stack -> Ok (q, (x - y):: stack)
+  | Mul :: q, x :: y :: stack -> Ok (q, (x * y):: stack)
+  | Div :: q, x :: y :: stack -> 
+    if y = 0 then Error ("Division by zero", state)
+    else Ok (q, (x / y):: stack)
+  | Rem :: q, x :: y :: stack -> 
+    if y= 0 then Error ("Remainder by zero", state)
+    else Ok (q, (x mod y):: stack)
+
+    (*runtime errors*)
+    | Pop :: _, _ -> Error ("Pop on empty stack", state)
+    | Swap :: _, _ -> Error ("Swap needs at least two elements", state)
+    | Add :: _, _ -> Error ("Add needs at least two elements", state)
+    | Sub :: _, _ -> Error ("Sub needs at least two elements", state)
+    | Mul :: _, _ -> Error ("MUl needs at least two elements", state)
+    | Div :: _, _ -> Error ("Div needs at least two elements", state)
+    | Rem :: _, _ -> Error ("Rem needs at least two elements", state)
+
 
 let eval_program (numargs, cmds) args =
   let rec execute = function
